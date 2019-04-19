b9330556f0a70cdbfbe8069410be0a1eba2ac7ca
ZakaNaji <zakaria.naji@edu.uca.ma>
2019-04-19 10:51:05 +0200

    FINISHED ANNOTATION EXO

--- src/Annotations/MainSolution.java
+++ src/Annotations/MainSolution.java
@@ -1,5 +1,9 @@
 package Annotations;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Scanner;
@@ -40,6 +44,8 @@ public class MainSolution {
 
 }
 
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
 @interface roleBudgetAnnotation{
     String role();
     int budget();
